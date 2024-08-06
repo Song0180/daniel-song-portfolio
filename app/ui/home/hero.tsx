912e97b935fcf711eff3dc065ecec0e4ed977c79
song0180 <song0180@e.ntu.edu.sg>
2024-08-06 22:52:34 +0800

    style: svg icon & favicon updates

--- app/ui/home/hero.tsx
+++ app/ui/home/hero.tsx
@@ -1,4 +1,5 @@
 import userData from '@/app/lib/data';
+import { CornerLeftUp } from 'lucide-react';
 import Image from 'next/image';
 
 export default function Hero() {
@@ -28,19 +29,7 @@ export default function Hero() {
           />
           <div className='flex flex-row justify-between mt-4'>
             <div className='flex flex-row space-x-4'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='16'
-                height='16'
-                fill='currentColor'
-                className='bi bi-arrow-90deg-up'
-                viewBox='0 0 16 16'
-              >
-                <path
-                  fillRule='evenodd'
-                  d='M4.854 1.146a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L4 2.707V12.5A2.5 2.5 0 0 0 6.5 15h8a.5.5 0 0 0 0-1h-8A1.5 1.5 0 0 1 5 12.5V2.707l3.146 3.147a.5.5 0 1 0 .708-.708l-4-4z'
-                />
-              </svg>
+              <CornerLeftUp className='w-4 h-4' />
               <p className='font-mono'>Me in Shanghai</p>
             </div>
           </div>
--- app/ui/home/latestRepos.tsx
+++ app/ui/home/latestRepos.tsx
@@ -1,6 +1,7 @@
 import { getLatestRepos } from '@/app/lib/actions';
 import userData from '@/app/lib/data';
 import { Repo } from '@/app/lib/definitions';
+import { ExternalLink, MoveRight } from 'lucide-react';
 
 export default async function LatestRepos() {
   const repos = await getLatestRepos(userData.githubUsername);
@@ -15,23 +16,9 @@ export default async function LatestRepos() {
 
           <a
             href={`https://github.com/${userData.githubUsername}`}
-            className='mb-20 md:mb-0 px-8 py-4 rounded-md bg-white shadow-lg text-xl font-semibold flex flex-row space-x-4 items-center dark:bg-zinc-900 dark:text-gray-300'
+            className='mb-20 md:mb-0 px-8 py-4 rounded-md bg-white hover:bg-zinc-100 dark:hover:bg-zinc-800 transition duration-200 shadow-lg text-xl font-semibold flex flex-row space-x-4 items-center dark:bg-zinc-900 dark:text-gray-300'
           >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='16'
-              height='16'
-              fill='currentColor'
-              className='bi bi-arrow-up-right-square'
-              stroke='4'
-              strokeWidth='4'
-              viewBox='0 0 16 16'
-            >
-              <path
-                fillRule='evenodd'
-                d='M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z'
-              />
-            </svg>
+            <ExternalLink />
             <p>View GitHub</p>
           </a>
         </div>
@@ -67,7 +54,7 @@ const GithubRepoCard = ({ latestRepo }: { latestRepo: Repo }) => {
       >
         <p>View Repository</p>
         <div className='transform group-hover:translate-x-2 transition duration-300'>
-          &rarr;
+          <MoveRight className='w-4 h-4' />
         </div>
       </a>
     </div>
